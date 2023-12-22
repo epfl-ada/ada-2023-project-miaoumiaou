1162938990cb0150388ba4b7f906a8b8e5e3d6c6
WassimMaj <wassim.maj@epfl.ch>
2023-12-22 11:01:49 +0100

    coloooooooors and functions in functions.py

--- Functions.py
+++ Functions.py
@@ -8,6 +8,17 @@ import requests
 from bs4 import BeautifulSoup 
 import os
 from Functions import *
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+from math import pi
+from plotly.subplots import make_subplots
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from scipy.stats import ttest_1samp
+from scipy.stats import chi2_contingency
+import plotly.figure_factory as ff
+from collections import Counter
 
 def download_extract_data():
     directory_path = "./data"
@@ -76,3 +87,101 @@ def load_plain_article():
 
     test_data = pd.DataFrame(file_data)
     return test_data
+
+
+# Function to convert bias labels to numbers, calculate averages, and then convert back to categorical bias
+def calculate_path_biases(path, all_articles_df):
+    # Define a mapping from categorical bias to numerical values
+    bias_mapping = {
+    'left': -1,
+    'center': 0,
+    'right': 1
+    }
+
+    articles = path.split(';')  # Adjust delimiter if needed
+    biases = all_articles_df[all_articles_df['article'].isin(articles)]['Bias']
+    
+    # Map the categorical biases to numerical values
+    numerical_biases = biases.map(bias_mapping)
+    
+    # Calculate the average numerical bias
+    average_numerical_bias = numerical_biases.mean()
+    # Get the bias of the start article, assuming the first article in the list
+    start_numerical_bias = numerical_biases.iloc[0] if len(numerical_biases) > 0 else None
+    #Get the bias of the end article, assuming the last article in the list
+    end_numerical_bias = numerical_biases.iloc[-1] if len(numerical_biases) > 0 else None
+
+    # Convert the average numerical bias back to a categorical bias
+    average_bias_label = numerical_to_categorical_bias(average_numerical_bias)
+    
+    # Convert the start numerical bias back to a categorical bias if necessary
+    start_bias_label = numerical_to_categorical_bias(start_numerical_bias) if start_numerical_bias is not None else None
+    #Convert the end numerical bias back to a categorical bias if necessary
+    end_bias_label = numerical_to_categorical_bias(end_numerical_bias) if end_numerical_bias is not None else None
+
+    return average_numerical_bias, average_bias_label, start_bias_label, end_bias_label
+
+# Helper function to convert numerical bias back to categorical
+def numerical_to_categorical_bias(numerical_bias):
+    if numerical_bias < -1/3:
+        return 'left'
+    elif -1/3 <= numerical_bias <= 1/3:
+        return 'center'
+    else:
+        return 'right'
+    
+
+    # Function to create histograms with colored bias zones
+def create_colored_histogram(df, title):
+    # Initialize figure
+    fig = go.Figure()
+
+    # Define colors for bias zones
+    colors = {
+        'left': 'lightcoral', 
+        'center': 'lightgray',  
+        'right': 'lightblue'  
+    }
+
+    # Add histogram for each bias zone with specific color
+    for label, color in colors.items():
+        # Filter the dataframe for the specific bias zone
+        zone_df = df[(df['average_numerical_bias'] > -1/3 if label != 'left' else df['average_numerical_bias'] <= -1/3) &
+                     (df['average_numerical_bias'] <= 1/3 if label != 'right' else df['average_numerical_bias'] > 1/3)]
+        # Add trace for the zone
+        fig.add_trace(go.Histogram(x=zone_df['average_numerical_bias'], name=label.capitalize(),
+                                   marker_color=color, nbinsx=30, opacity=0.6))
+
+    # Add vertical lines to demarcate the bias zones
+    fig.add_vline(x=-1/3, line_width=2, line_dash="dash", line_color="grey")
+    fig.add_vline(x=1/3, line_width=2, line_dash="dash", line_color="grey")
+
+    # Update layout
+    fig.update_layout(title=title, xaxis_title='Average Numerical Bias', yaxis_title='Count',
+                      barmode='stack')
+
+    # Add annotations for start and end counts in each zone
+    for label, color in colors.items():
+        start_count = df[df['start_bias_label'] == label].shape[0]
+        end_count = df[df['end_bias_label'] == label].shape[0]
+        # Place annotation in the middle top of the graph in their respective zone
+        zone_center = {'left': 0.2, 'center': 0.5, 'right': 0.8}[label]
+        fig.add_annotation(x=zone_center, y=1.02, text=f"Start: {start_count}<br>End: {end_count}",
+                        showarrow=False, xref="paper", yref="paper", bgcolor=color, font=dict(color='black'),
+                        align="center")
+    return fig
+
+
+# Define a function to assign color based on bias value
+def get_color(value):
+    colors = {
+        'left': 'lightcoral', 
+        'center': 'lightgray',  
+        'right': 'lightblue'  
+    }
+    if value < -1/3:
+        return colors['left']
+    elif value > 1/3:
+        return colors['right']
+    else:
+        return colors['center']
